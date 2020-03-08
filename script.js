eb7cfca9f48fc976689d733d4381d12b3b43dced
lutfilhadimohdafez <61901435+lutfilhadimohdafez@users.noreply.github.com>
2020-03-08 14:56:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ class Calculator{
     }
 
     chooseOperation(operation){
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== ''){
+            this.compute()
+        }
         this.operation = operation
         this.previousOperand = this.currentOperand
         this.currentOperand = ''
